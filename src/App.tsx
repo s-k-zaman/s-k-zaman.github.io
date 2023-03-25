0471adb4139774df0e95eae55b42c5f6f108f12b
s-k-zaman <sudo.coder.zaman@gmail.com>
2023-03-25 14:08:39 +0530

    font files added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { useState } from 'react'
 function App() {
 
     return (
-        <div className="App">
-            <h1 className='font-bold text-xl '>
+        <div className="App flex flex-col items-center justify-center min-h-screen bg-primary">
+            <h1 className='font-minecraft font-bold text-xl capitalize text-pink-600 shadow-md border p-1 px-3 animate-pulse'>
                 under Construction
             </h1>
-
-
         </div>
     )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        minecraft: ["minecraft"]
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#09192F",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
