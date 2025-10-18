4309aa4c329d15766feb15df6d09be2fee0da82d
s-k-zaman <sudo.coder.zaman@gmail.com>
2025-10-18 15:07:44 +0530

    add: mail icon

--- src/components/rightMail/index.tsx
+++ src/components/rightMail/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { FiMail } from "react-icons/fi";
 
 function RightMail() {
   return (
-    <div className="fixed bottom-0 right-[-55px] text-secondary-text hidden xl:flex flex-col items-center gap-[20px]">
+    <div className="fixed bottom-0 right-[-55px] text-secondary-text hidden xl:flex flex-col items-center gap-[31px]">
       <div className="rotate-90 -translate-y-[110px]">
         <a
           href="mailto:sudo.coder.zaman@gmail.com"
           target="_blank"
-          className="text-primary-bar font-minecraft font-medium tracking-[0.1em] cursor-pointer hover:text-white"
+          className="text-primary-bar font-minecraft font-medium tracking-[0.1em] cursor-pointer hover:text-white flex items-center gap-2"
         >
-          sudo.coder.zaman<span className="text-primary-bar/70 text-lg">@</span>
-          gmail.com
+          <FiMail />
+          <div>
+            sudo.coder.zaman
+            <span className="text-primary-bar/70 text-lg">@</span>
+            gmail.com
+          </div>
         </a>
       </div>
       <div className="border-[2px] border-primary-bar h-[190px] w-0"></div>
